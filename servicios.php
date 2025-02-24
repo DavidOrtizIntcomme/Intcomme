a39a5e1660c3ced7eeeb62dcdd9bbdf8531d6cc9
DavidOrtizIntcomme <david.ortiz@intcomme.com>
2025-02-24 16:49:55 -0700

    Página de servicios

---
+++ servicios.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es"> 
+
+    <?php require_once "componentes/head.php"; ?>
+
+    <body>
+
+        <?php require_once "componentes/menu.php"; ?>
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <h1 class="display-1 text-white animated slideInDown">SERVICIOS INTCOMME</h1>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+    <!-- Service Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                <h4 class="section-title">Nuestros servicios</h4>
+                <h3 class="mb-4">Intcomme ofrece una gran variedad de servicios logísticos de valor que nos permiten cubrir todas las necesidades de nuestros clientes.</h3>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-1.png" alt="Icon">
+                            <h3 class="mb-3">Servicios logísticos de importación y exportación</h3>
+                            <p class="mb-4">Nuestros servicios logísticos integrales permiten exportar e importar mercancías de todo tipo con seguridad eficacia y puntualidad. En Intcomme Logistics garantizamos estándares de calidad idóneos que eficientán las operaciones de nuestros clientes.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-6.png" alt="Icon">
+                            <h3 class="mb-3">Servicios logísticos gestión de transporte</h3>
+                            <p class="mb-4">Trabajamos con los mejores socios comerciales, soluciónanos en tándem la distribución y tránsito de cada operación.Gestionamos transportación para todo tipo de negocio sin importar destino, volumen y tamaño de manera estratégica. Incluyendo transporte terrestre, marítimo y aéreo.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-3.png" alt="Icon">
+                            <h3 class="mb-3">Almacenaje</h3>
+                            <p class="mb-4">Contamos con almacenes logísticos modernos y con todas las medidas de seguridad garantizando el almacenamiento y manipulación de su carga en todo momento. Nos encargamos del almacenamiento, manipulación, clasificación, desconsolidación, distribución y carga en cualquier tipo de contenedor o transporte.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-4.png" alt="Icon">
+                            <h3 class="mb-3">CROSS DOCKING</h3>
+                            <p class="mb-4">Intcomme ofrece la posibilidad de almacenaje para envíos de carga consolidada en nuestras plataformas de cross-docking. Las mercancías de nuestros clientes se almacenan en una zona de cross-docking a la espera de consolidar mercancía y preparar para entrega.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-5.png" alt="Icon">
+                            <h3 class="mb-3">ADMINISTRACIÓN ADUANAL</h3>
+                            <p class="mb-4">Nuestros profesionales de comercio exterior, están actualizados en las normativas que son parte de cada operación, regulaciones y leyes. Entendemos que la efectividad, seguridad y tranquilidad en las operaciones logísticas dependen en gran parte del servicio de administración aduanal.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item d-flex position-relative text-center h-100">
+                        <div class="service-text p-5">
+                            <img class="mb-4" src="img/icons/icon-6.png" alt="Icon">
+                            <h3 class="mb-3">COMPRAMOS PARA TU EMPRESA EN USA</h3>
+                            <p class="mb-4">Podemos comprar para tu empresa en USA, Estados Unidos e importarlos a México hasta tu puerta.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Service End -->
+
+    <!-- Testimonial Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                <h4 class="section-title">CUÉNTANOS ACERCA DE TU PROYECTO Y TU NEGOCIO…</h4>
+                <h1 class="display-5 mb-4">¿ESTAS LISTO PARA TRABAJAR CON NOSOTROS?</h1>
+                <a href="contacto" class="btn btn-primary py-3 px-5 animated slideInLeft">COTIZA TU PROYECTO</a>
+            </div>     
+        </div>
+    </div>
+    <!-- Testimonial End -->
+    
+    <?php require_once "componentes/footer.php"; ?>
+
+    </body>
+
+    <?php require_once "componentes/scripts-js.php"; ?>
+
+</html>
